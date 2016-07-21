f93f997f9b55ddcb701005fdcd27c1407c1b4e83
root <root@iZ943gt60hvZ.(none)>
2016-07-21 23:56:20 +0800

    update

--- protocols.js
+++ protocols.js
@@ -204,13 +204,10 @@ function checkSearchOpponent(obj){
 
 function queryUserValue(userid,callback){
     sql = 'select * from t_pushup_user where userid=?'
-    console.log('userid type:',userid,typeof(userid));
     db.query(sql, [userid], function(err, rows){
         if(err){
             callback({'errno':-1,'error':err.msg});
         }else{
-            console.log(rows);
-            console.log(rows.length);
             if(rows.length==1){
                 myvalue = rows[0]['value'];
                 if(myvalue==0)
